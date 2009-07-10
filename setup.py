bddc7a8c983c5a4b3b4760eb97e08bb2ef6cb391
Viktor Kotseruba <barbuzaster@gmail.com>
2009-07-10 16:33:17 +0400

    setup.py for lorien

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='supercaptcha',
+    version='',
+    packages=['supercaptcha'],
+    package_data={
+        '':['fonts/*.ttf']
+    },
+    install_requires=[
+        'django',
+        'PIL'
+    ],
+    author='Viktor Kotseruba',
+    author_email='barbuzaster@gmail.com',
+    description='captchafield for django newforms',
+    license='MIT',
+    keywords='forms web django',
+)
